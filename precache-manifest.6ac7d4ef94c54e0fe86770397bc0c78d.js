a52510159b72aa1d0031fd5071c85687699d63d1
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-09-28 09:47:10 -0500

    Updates

---
+++ precache-manifest.6ac7d4ef94c54e0fe86770397bc0c78d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c279c74e68050c973ab85c83d6b5d0",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "143dabd1b7d6117a9d58",
+    "url": "/portafolio/static/css/main.c00bb588.chunk.css"
+  },
+  {
+    "revision": "52a54b1a468016e79a38",
+    "url": "/portafolio/static/js/2.a4de6faf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio/static/js/2.a4de6faf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143dabd1b7d6117a9d58",
+    "url": "/portafolio/static/js/main.d0ddf308.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  },
+  {
+    "revision": "444a474a32689e4e45d2cc7b07668517",
+    "url": "/portafolio/static/media/async.444a474a.jpg"
+  },
+  {
+    "revision": "af96fa3c8f496b8fee606b071fc3a237",
+    "url": "/portafolio/static/media/badges.af96fa3c.jpg"
+  },
+  {
+    "revision": "8e222b3b4816afe1d02a29605e40d29b",
+    "url": "/portafolio/static/media/basica.8e222b3b.jpg"
+  },
+  {
+    "revision": "8da90dc567bdc53f592f989c7d278f12",
+    "url": "/portafolio/static/media/basico.8da90dc5.jpg"
+  },
+  {
+    "revision": "0a559d48a48e850028b3a2582061c00f",
+    "url": "/portafolio/static/media/desarrollo.0a559d48.jpg"
+  },
+  {
+    "revision": "2b778913c2dbe022452f60e23a6f37c9",
+    "url": "/portafolio/static/media/es6.2b778913.jpg"
+  },
+  {
+    "revision": "bd0d6a0976e3caf0f75c92a43af8da91",
+    "url": "/portafolio/static/media/escritura.bd0d6a09.jpg"
+  },
+  {
+    "revision": "5dd8db76d067328fbf2c9d63584a6651",
+    "url": "/portafolio/static/media/frontend.5dd8db76.jpg"
+  },
+  {
+    "revision": "79301e39aaf8c1d682afe30393cee123",
+    "url": "/portafolio/static/media/fundamentos.79301e39.jpg"
+  },
+  {
+    "revision": "a00b68e880b6234988326e3ddc26d6e8",
+    "url": "/portafolio/static/media/gestion.a00b68e8.jpg"
+  },
+  {
+    "revision": "012f5ae7d6eda7cdce23fdf1a7a463c7",
+    "url": "/portafolio/static/media/git.012f5ae7.jpg"
+  },
+  {
+    "revision": "c75dcc30d272c69ab0b15cbae3ecc16c",
+    "url": "/portafolio/static/media/ingles-practico.c75dcc30.jpg"
+  },
+  {
+    "revision": "04b75597b1c0f49abf2187e971d70aeb",
+    "url": "/portafolio/static/media/ingles-tecnico.04b75597.jpg"
+  },
+  {
+    "revision": "61c39af0fb54dcb807266ed602b0696a",
+    "url": "/portafolio/static/media/jquery.61c39af0.jpg"
+  },
+  {
+    "revision": "4e1bf4016bc3dcf859b233e588c7e99a",
+    "url": "/portafolio/static/media/loguito.4e1bf401.jpg"
+  },
+  {
+    "revision": "e51d1e172c31d17e3e2cdef6c3334500",
+    "url": "/portafolio/static/media/me.e51d1e17.jpg"
+  },
+  {
+    "revision": "c4adb1a7479fb4483e9744478395882f",
+    "url": "/portafolio/static/media/poke.c4adb1a7.jpg"
+  },
+  {
+    "revision": "866cdaac7dfc3d9f620dda3c7bd6c49e",
+    "url": "/portafolio/static/media/prework.866cdaac.jpg"
+  },
+  {
+    "revision": "f81bcc57ae609fc8b7dfb0a719ebbd0d",
+    "url": "/portafolio/static/media/profesional.f81bcc57.jpg"
+  },
+  {
+    "revision": "23a8739af84dfa6daf367d3290a561ee",
+    "url": "/portafolio/static/media/pvideo.23a8739a.jpg"
+  },
+  {
+    "revision": "41f64e3ec9bf2c17f505d708aa9501a0",
+    "url": "/portafolio/static/media/react.41f64e3e.jpg"
+  },
+  {
+    "revision": "ef64d65146822409f0b460b3122c5420",
+    "url": "/portafolio/static/media/reactjs.ef64d651.jpg"
+  },
+  {
+    "revision": "10994704892a8440fcb22356d773ec77",
+    "url": "/portafolio/static/media/rick.10994704.jpg"
+  },
+  {
+    "revision": "691fc7b8738ffea078459eafba98029a",
+    "url": "/portafolio/static/media/terminal.691fc7b8.jpg"
+  }
+]);
