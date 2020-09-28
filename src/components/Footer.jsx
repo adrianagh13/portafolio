9e62d2801a1672779aae90fefccb12ebc491333e
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-09-28 09:42:35 -0500

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './styles/Footer.css'
+import fb from '../images/facebook.png'
 
 const Footer = () => (
     <section id="contacto" className="contact">
@@ -12,9 +13,9 @@ const Footer = () => (
             </form>
             <div className="social">
                 <h3>Encuéntrame en mis redes sociales :)</h3>
-                <a href="https://facebook.com/adrianaguadalupe.gh" className="social-link facebook" target="_blank" ></a>
-                <a href="https://github.com/adrianagh13" className="social-link github" target="_blank"></a>
-                <a href="https://instagram.com/adriannggh" className="social-link instagram" target="_blank"></a>
+                <a href="https://facebook.com/adrianaguadalupe.gh" className="social-link facebook" target="_blank" style={{ backgroundImage: `url(${require('../images/facebook.png')})`}}></a>
+                <a href="https://github.com/adrianagh13" className="social-link github" target="_blank" style={{ backgroundImage: `url(${require('../images/github.png')})`}}></a>
+                <a href="https://instagram.com/adriannggh" className="social-link instagram" target="_blank" style={{ backgroundImage: `url(${require('../images/instagram.png')})`}}></a>
             </div>
         </div>
     </section>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import './styles/Portfolio.css';
 
 import poke from '../images/poke.jpg'
-import video from '../images/video.jpg'
+import rick from '../images/rick.jpg'
+import pvideo from '../images/pvideo.jpg'
+import badges from '../images/badges.jpg'
 
 const Portfolio = () => (
     <section id="portafolio" className="portfolio">
@@ -26,7 +28,7 @@ const Portfolio = () => (
                     <p className="project-description">Web App que muestra a todos los personajes de la serie Rick y Morty. <br />Los datos fueron extraídos de la <a href="https://rickandmortyapi.com/"> API de Rick and Morty</a>. <br />Desarrollada con HTML, CSS, Bootstrap y React.</p>
                 </div>
                 <figure className="project-imageContainer">
-                    <img className="project-image" src={video} alt="video" width="500" height="300" />
+                    <img className="project-image" src={rick} alt="video" width="500" height="300" />
                 </figure>
             </article>
             <article className="project">
@@ -36,17 +38,17 @@ const Portfolio = () => (
                     <p className="project-description">La función de esta aplicación es gestionar los registros de usuarios asistentes a un evento, permite filtrar datos, editar y borrar registros. <br />Desarrollada con React, HTML, CSS y Bootstrap.</p>
                 </div>
                 <figure className="project-imageContainer">
-                    <img className="project-image"src={video} alt="video" width="500" height="300" />
+                    <img className="project-image"src={badges} alt="video" width="500" height="300" />
                 </figure>
             </article>
             <article className="project">
                 <div className="project-details"> 
                     <h3 className="project-title">Platzi Video</h3>
-                    <p className="project-url"><strong>Chécala en: </strong>https://adrianagh13.github.io/Pokemon/</p>
-                    <p className="project-description">Se trata de una plataforma de reproducción de videos que te permite registarte como usuario y crear tus propias listas de videos favoritos.<br />Desarrollada con HTML, CSS, Bootstrap y React.</p>
+                    <p className="project-url"><strong>Checa el repositorio: </strong><a target="_blank" href="https://github.com/adrianagh13/platzi-video-react">Aquí</a></p>
+                    <p className="project-description">(En mantenimiento) Se trata de una plataforma de reproducción de videos que te permite registarte como usuario y crear tus propias listas de videos favoritos.<br />Desarrollada con HTML, CSS, Bootstrap y React.</p>
                 </div>
                 <figure className="project-imageContainer">
-                    <img className="project-image"src={video} alt="video" width="500" height="300" />
+                    <img className="project-image"src={pvideo} alt="video" width="500" height="300" />
                 </figure>
             </article>
             
